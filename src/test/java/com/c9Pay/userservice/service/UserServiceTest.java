4cfd3e9c78ba96a49999841764461057aba6e532
minjong <koust6u@pusan.ac.kr>
2023-06-28 18:07:52 +0900

    test: 아이디 중복확인 테스트 코드 추가
    * 본문 ---

---
+++ src/test/java/com/c9Pay/userservice/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package com.c9Pay.userservice.service;
+
+import com.c9Pay.userservice.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    @Transactional
+    void signUp_DuplicateUser_ExceptionThrown() {
+        //given
+        User user = User.builder()
+                .userId("aaa")
+                .credit(0L)
+                .username("testName")
+                .password("pass")
+                .email("aa@gmail.com")
+                .build();
+        //when
+        userService.signUp(user);
+
+        //then
+        assertThrows(IllegalStateException.class, () -> userService.signUp(user));
+    }
+}
