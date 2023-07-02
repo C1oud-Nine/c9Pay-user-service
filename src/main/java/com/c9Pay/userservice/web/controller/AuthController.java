fa85db4b5cd79a25f8daf4237f370c4c3c0f6b61
minjong kim <godminjong@naver.com>
2023-07-02 15:16:47 +0900

    feat: RSA 서명생성, 검증 기능
    * RSA 암/복호화 및 서명 생성/검증 기능 추가함 ---

--- src/main/java/com/c9Pay/userservice/web/controller/AuthController.java
+++ src/main/java/com/c9Pay/userservice/web/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.c9Pay.userservice.web.controller;
 
+import com.c9Pay.userservice.certificate.Certificate;
 import com.c9Pay.userservice.certificate.CertificateProvider;
 import com.c9Pay.userservice.web.dto.AuthForm;
+import com.c9Pay.userservice.web.dto.CertificateForm;
+import com.c9Pay.userservice.web.dto.CertificateResponse;
 import com.c9Pay.userservice.web.dto.PublicKeyResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.nio.charset.StandardCharsets;
 import java.security.PublicKey;
 import java.util.Base64;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,4 +38,23 @@ public class AuthController {
         return ResponseEntity.ok(new PublicKeyResponse(base64Encoded));
     }
 
+    @PostMapping("/certificate")
+    public ResponseEntity<CertificateResponse> createCertificate(@RequestBody CertificateForm form){
+        Optional<Certificate> nullable = certificateProvider.getCertificate(form);
+
+        return nullable
+                .map(certificate ->
+                        ResponseEntity.ok(new CertificateResponse(certificate.getCertificate(), certificate.getSign())))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> verify(@RequestBody Certificate cert){
+        Optional<CertificateForm> nullable = certificateProvider.decrypt(cert, CertificateForm.class);
+
+        return nullable
+                .map(certificateForm ->
+                        ResponseEntity.ok(new CertificateForm(certificateForm.getSerialNumber())))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
 }
