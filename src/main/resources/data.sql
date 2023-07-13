84c00f4388c7b4faef55403ae24cd7cef2734dc5
minjong kim <godminjong@naver.com>
2023-07-13 22:36:51 +0900

    feat: 초기화 insert 쿼리 자동 생성 파일, property 추가
    * user entity에 대한 초기화 insert 쿼리 5개 자동 생성 파일 작성---

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (username, user_id, password, email, serial_number, credit)
+VALUES ('user1', 'aa', 'bb', 'user1@example.com', 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee', 1000);
+
+INSERT INTO users (username, user_id, password, email, serial_number, credit)
+VALUES ('user2', 'user2_id', 'password2', 'user2@example.com', '11111111-2222-3333-4444-555555555555', 2000);
+
+INSERT INTO users (username, user_id, password, email, serial_number, credit)
+VALUES ('user3', 'user3_id', 'password3', 'user3@example.com', '66666666-7777-8888-9999-000000000000', 3000);
+
+INSERT INTO users (username, user_id, password, email, serial_number, credit)
+VALUES ('user4', 'user4_id', 'password4', 'user4@example.com', 'abcdef12-3456-7890-efab-cdef12345678', 4000);
+
+INSERT INTO users (username, user_id, password, email, serial_number, credit)
+VALUES ('user5', 'user5_id', 'password5', 'user5@example.com', '98765432-1098-7654-3210-987654321098', 5000);
