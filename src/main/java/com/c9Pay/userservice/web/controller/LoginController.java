5a728dd62ef39d6f4a909069eaeba1ffe8445b2f
minjong kim <godminjong@naver.com>
2023-07-13 13:38:10 +0900

    feat: 로그인 기능 기본구현
    * 로그인 기능 기본 구현, userId를 바탕으로 비밀번호 확인 후
     jwt 생성 cookie에 포함 시킨다.---

--- src/main/java/com/c9Pay/userservice/web/controller/LoginController.java
+++ src/main/java/com/c9Pay/userservice/web/controller/LoginController.java
@@ -1,28 +1,41 @@
 package com.c9Pay.userservice.web.controller;
 
 
+import com.c9Pay.userservice.entity.User;
+import com.c9Pay.userservice.jwt.TokenProvider;
 import com.c9Pay.userservice.web.dto.LoginForm;
+import com.c9Pay.userservice.web.service.UserService;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @Slf4j
 @RestController
 @RequestMapping
+@RequiredArgsConstructor
 public class LoginController {
 
-    @PostMapping("/login")
-    public void login(@RequestBody LoginForm form, HttpServletResponse response) throws IOException {
-        Cookie auth = new Cookie("Auth", "dummyCookie");
-        response.addCookie(auth);
-        //@TODO spring security에서 AuthenticationSuccessHandler 구현 필요
-        response.sendRedirect("/user-service/user");
-    }
+    private final TokenProvider tokenProvider;
+    private final UserService userService;
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginForm form,  HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try{
+            Long id = userService.authenticate(form.getUserId(), form.getPassword());
+            String token = tokenProvider.createToken(id);
+            response.addCookie(new Cookie("Authorization", "Bearer+"+token));
+            log.debug("token: {}", token);
+            return ResponseEntity.ok("로그인 성공");
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
 }
