02794388c8c1d02e3737686cc6ec05b9f9f4411d
minjong kim <godminjong@naver.com>
2023-07-13 23:04:09 +0900

    feat: user entity CRUD 기능 추가
    * 사용자 정보 수정, 탈퇴, 조회 기능을 추가함---

--- src/main/java/com/c9Pay/userservice/web/repository/UserRepository.java
+++ src/main/java/com/c9Pay/userservice/web/repository/UserRepository.java
@@ -2,14 +2,13 @@ package com.c9Pay.userservice.web.repository;
 
 
 import com.c9Pay.userservice.entity.User;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(String UserId);
+    Optional<User> findById(Long id);
+
 }
+
