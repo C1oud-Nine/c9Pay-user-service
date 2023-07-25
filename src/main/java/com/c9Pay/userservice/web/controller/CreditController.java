ae1e0bf98ed907d187c6bafd7183e59f8be8cdcb
minjong kim <godminjong@naver.com>
2023-07-25 14:18:12 +0900

    fix: end-point  api/internal 수정
    *잘못 작성된 api/ internal 구분 수정---

--- src/main/java/com/c9Pay/userservice/web/controller/CreditController.java
+++ src/main/java/com/c9Pay/userservice/web/controller/CreditController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/internal/credit")
+@RestController("/api/credit")
 public class CreditController {
-    @PostMapping("/credit")
+    @PostMapping
     public ResponseEntity<?> chargeCredit(@RequestBody ChargeDetail charge, @CookieValue("Auth")String auth){
         //@TODO: feign-client(credit-service: 크레딧 충전 요청)
         return ResponseEntity.ok().build();
--- src/main/java/com/c9Pay/userservice/web/controller/QrController.java
+++ src/main/java/com/c9Pay/userservice/web/controller/QrController.java
@@ -27,7 +27,7 @@ public class QrController {
     private final CertificateProvider certificateProvider;
     private final TokenProvider tokenProvider;
     private final UserService userService;
-    @GetMapping("/internal/qr")
+    @GetMapping("/api/qr")
     public ResponseEntity<CertificateResponse> createQr(@CookieValue(AUTHORIZATION_HEADER) String token){
 
         Authentication authentication = tokenProvider.getAuthentication(token.substring(7));
