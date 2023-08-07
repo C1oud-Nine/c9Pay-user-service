07294ee4f64e5a273d599ecf0cb34fa4d6211a1e
minjong <koust6u@pusan.ac.kr>
2023-08-08 05:59:25 +0900

    test: open feign client test 코드작성
    * open feign 관련 외부  api 호출 테스트 코드 작성---

--- src/main/java/com/c9Pay/userservice/web/client/AuthClient.java
+++ src/main/java/com/c9Pay/userservice/web/client/AuthClient.java
@@ -5,8 +5,10 @@ import com.c9Pay.userservice.data.dto.auth.CertificateResponse;
 import com.c9Pay.userservice.data.dto.auth.SerialNumberResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="auth-service", url="${AUTH_SERVICE_URI:http://localhost}:8081")
 public interface AuthClient {
@@ -15,6 +17,9 @@ public interface AuthClient {
     ResponseEntity<SerialNumberResponse> getSerialNumber();
 
     @PostMapping("/auth-service/certificate")
-    ResponseEntity<CertificateResponse> getCertificate(@RequestBody CertificateForm certificateForm);
+    ResponseEntity<CertificateResponse> getCertificate( CertificateForm certificateForm);
 
+
+    @GetMapping("/auth-service/serial-number")
+    ResponseEntity<?> validateSerialNumber(@RequestParam String serialNumber);
 }
--- src/main/java/com/c9Pay/userservice/web/client/CreditClient.java
+++ src/main/java/com/c9Pay/userservice/web/client/CreditClient.java
@@ -1,23 +1,24 @@
 package com.c9Pay.userservice.web.client;
 
+import com.c9Pay.userservice.data.dto.credit.AccountDetails;
 import com.c9Pay.userservice.data.dto.credit.ChargeForm;
-import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @FeignClient(name="credit-service", url="${CREDIT_SERVICE_URI:http://localhost}:8082")
 public interface CreditClient {
     @PostMapping("/credit-service/account/{identifier}")
-    ResponseEntity<?> createAccount(@PathVariable String identifier);
+    ResponseEntity<?> createAccount(@PathVariable("identifier") String identifier);
 
 
-    @Headers("Accept: application/json")
-    @PostMapping(value = "/credit-service/account/{identifier}/load",  produces = "application/json")
-    ResponseEntity<?> loadCredit(@PathVariable String identifier, ChargeForm form);
+    @PostMapping("/credit-service/account/{identifier}/load")
+    ResponseEntity<?> loadCredit(@PathVariable("identifier") String identifier, ChargeForm form);
 
     @DeleteMapping("/credit-service/account/{identifier}")
-    ResponseEntity<?> deleteAccount(@PathVariable String identifier);
+    ResponseEntity<?> deleteAccount(@PathVariable("identifier") String identifier);
+
+    @GetMapping("/credit-service/account/{identifier}")
+    ResponseEntity<AccountDetails> getAccount(@PathVariable("identifier") String identifier);
 }
