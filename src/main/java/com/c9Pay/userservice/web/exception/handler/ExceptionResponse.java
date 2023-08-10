1bd9d6f962caaf03759487c7ed57b57ec03fe354
minjong <koust6u@pusan.ac.kr>
2023-08-10 21:56:21 +0900

    style: 패키지 구조 개선으로 인해 import 수정
    *  패키지 구조 개선으로 인해 import 수정 ---

--- src/main/java/com/c9Pay/userservice/web/exception/handler/ExceptionResponse.java
+++ src/main/java/com/c9Pay/userservice/web/exception/handler/ExceptionResponse.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Data
 @NoArgsConstructor
@@ -17,11 +15,15 @@ public class ExceptionResponse {
     private String message;
     private String details;
 
-    private List<String> field = new ArrayList<>();
+    private Map<String, String> field = new HashMap<>();
 
     public ExceptionResponse(Date timeStamp, String message, String details) {
         this.timeStamp = timeStamp;
         this.message = message;
         this.details = details;
+    }
+
+    public void addValidation(String fieldName, String errorMessage){
+        field.put(fieldName, errorMessage);
     }
 }
